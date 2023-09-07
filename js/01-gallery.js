661386125e46ce1c4bcaaa042bd0ee9bf61fed59
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-09-08 00:20:43 +0200

    instance.show

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.dir(galleryItems);
+
+const galleryUl = document.querySelector(".gallery")
+
+let markup = galleryItems.map(({ description, original, preview }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("")
+
+galleryUl.insertAdjacentHTML("beforeend", markup)   
+
+galleryUl.addEventListener("click", onClick)
+function onClick(event) {
+   if (event.currentTarget===event.target) {
+    return
+   }
+    console.log(event.target.dataset.source);
+    console.dir(event.target.alt);
+    const instance = basicLightbox.create(` 
+// <div>
+//     <img
+            class="gallery__image"
+            src="${event.target.dataset.source}" 
+            alt="event.target.alt">
+// </div>
+// `)
+
+    instance.show()
+
+    
+}
